f527ac574d0239ae6718260f285c5a0512de9cd3
RitaRez <ritarezborges@gmail.com>
2018-03-07 17:51:48 -0300

    Agora funciona :)

--- app/src/main/java/com/example/a2016951715/android_e02_activity/MainActivity.java
+++ app/src/main/java/com/example/a2016951715/android_e02_activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.a2016951715.android_e02_activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,22 +18,22 @@ public class MainActivity extends DebugActivity {
         setContentView(R.layout.activity_main);
         Log.i(MainActivity.class.getName(), "onCreate()");
 
-        final EditText user = findViewById(R.id.username);
-        final EditText psw = findViewById(R.id.password);
+        final EditText usuario = findViewById(R.id.username);
+        final EditText senha = findViewById(R.id.password);
         Button button = findViewById(R.id.btn);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (user.getText().toString().equals("admin") && psw.getText().toString().equals("admin123")){
-                    Intent intent = new Intent(MainActivity.this, afterLogin.class);
+                if ( senha.getText().toString().equals("admin123") && usuario.getText().toString().equals("admin") ){
                     Bundle args = new Bundle();
-                    args.putCharSequence("usuario", user.getText().toString());
-                    args.putCharSequence("senha", psw.getText().toString());
+                    args.putCharSequence("usuario", usuario.getText().toString());
+                    args.putCharSequence("senha", senha.getText().toString());
+                    Intent intent = new Intent(MainActivity.this, afterLoginActivity.class);
                     intent.putExtras(args);
                     startActivity(intent);
                 }else{
-                    Toast.makeText(MainActivity.this, "Dados incorretos", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Login ou senha errados", Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/example/a2016951715/android_e02_activity/afterLoginActivity.java
+++ app/src/main/java/com/example/a2016951715/android_e02_activity/afterLoginActivity.java
@@ -1,25 +1,23 @@
 package com.example.a2016951715.android_e02_activity;
 
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-public class afterLogin extends DebugActivity {
+public class afterLoginActivity extends DebugActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_login);
-        Log.i(afterLogin.class.getName(), "onCreate()");
+        Log.i(afterLoginActivity.class.getName(), "onCreate()");
 
         Bundle activityBundle = this.getIntent().getExtras();
         String user = activityBundle.getString("usuario");
         String password = activityBundle.getString("senha");
         TextView lblMessage = findViewById(R.id.msg);
-        lblMessage.setText("Bem vindo, " + user + "!\nSua senha é " + password);
+        lblMessage.setText("Bem vindo, " + user + "!\nA sua senha é " + password);
     }
 
 
