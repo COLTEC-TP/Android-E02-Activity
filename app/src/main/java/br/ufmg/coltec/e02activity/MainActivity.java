b2ebc2e9e46997a95cb1f517f8b0695a725c3521
kingnowdo <reinaldomagas@gmail.com>
2018-03-07 17:14:21 -0300

    Entendendo melhor o Ciclo de Vida

--- app/src/main/java/br/ufmg/coltec/e02activity/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/e02activity/MainActivity.java
@@ -1,13 +1,42 @@
 package br.ufmg.coltec.e02activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DebugActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText user = findViewById(R.id.inputUser);
+        final EditText pass = findViewById(R.id.inputPass);
+        Button loginButton = findViewById(R.id.inputButton);
+
+        final String userName = new String("admin");
+        final String password = new String("admin123");
+
+        loginButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(user.getText().toString().equals(userName) && pass.getText().toString().equals(password)){
+                            Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this,"Usuário ou senha incorreto",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
     }
 }
