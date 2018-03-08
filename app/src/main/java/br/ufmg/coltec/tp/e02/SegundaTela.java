6accc6db3d8fe8ac7e5bc8dae6b85dceb35e7630
MariSantos17 <mariana@luarinformatica.com.br>
2018-03-08 12:34:45 -0300

    Entendendo melhor o Ciclo de Vida Pronto

---
+++ app/src/main/java/br/ufmg/coltec/tp/e02/SegundaTela.java
@@ -0,0 +1,13 @@
+package br.ufmg.coltec.tp.e02;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SegundaTela extends DebugActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_tela);
+    }
+}
