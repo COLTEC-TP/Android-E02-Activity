bfca670da81f4822480a48fb47fb802b6228e8ef
Mateus3578 <76887902+Mateus3578@users.noreply.github.com>
2021-06-06 16:30:48 -0300

    Ajustes
    +Corrigindo erro do graddle
    +Criação das activities Debug e Login

---
+++ app/src/main/java/br/ufmg/coltec/e02_activity/DebugActivity.java
@@ -0,0 +1,14 @@
+package br.ufmg.coltec.e02_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+    }
+}
--- app/src/main/java/br/ufmg/coltec/e02_activity/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/e02_activity/MainActivity.java
@@ -1,6 +1,7 @@
-package br.ufmg.coltec.tp.e02;
+package br.ufmg.coltec.e02_activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
